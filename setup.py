272310fdb7d9e7ca7870749a893c0d888ef1f526
Tareque Hossain <tareque@codexn.com>
2017-06-12 18:37:23 -0400

    Fixed readme file name in manifest & setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.2.4'
 setup(name='mfabrik.zoho',
       version=version,
       description="Zoho API integration for Python",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
